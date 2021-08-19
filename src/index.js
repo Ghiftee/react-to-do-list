6ac43f2a19f81f25f783f06a37ea0fb6b9300dab
Ghiftee <gigibetine@gmail.com>
2021-08-19 09:42:48 +0100

    Fix linter errors in index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter as Router } from "react-router-dom";
-import TodoContainer from "./components/TodoContainer";
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import TodoContainer from './components/TodoContainer';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
-    </Router>  
-  </React.StrictMode>, 
-  document.getElementById("root")
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
